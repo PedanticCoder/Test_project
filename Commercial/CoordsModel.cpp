9610a988a138ac866b2d4ea2cb4e2f08ff219060
Artem <kurov.artem308@gmail.com>
2020-06-06 19:19:37 +0300

    add model base implementation

--- Commercial/CoordsModel.cpp
+++ Commercial/CoordsModel.cpp
@@ -1,6 +1,43 @@
 #include "CoordsModel.h"
+#include <QDebug>
 
 CoordsModel::CoordsModel()
 {
+}
 
+CoordsModel::~CoordsModel()
+{
+}
+
+int CoordsModel::rowCount(const QModelIndex &parent) const
+{
+    Q_UNUSED(parent);
+    return static_cast<int>(m_coordinates.size());
+}
+
+int CoordsModel::columnCount(const QModelIndex &parent) const
+{
+    Q_UNUSED(parent);
+    return 2;
+}
+
+QVariant CoordsModel::data(const QModelIndex &index, int role) const
+{
+    if (!index.isValid() || index.row() > rowCount(index)) {
+        return {};
+    }
+
+    const std::pair<int, int> coords = m_coordinates.at(index.row());
+
+    switch (role) {
+        case CoordRoles::XCoord: {
+            return QVariant::fromValue(coords.first);
+        }
+        case CoordRoles::YCoord: {
+            return QVariant::fromValue(coords.second);
+        }
+        default: {
+            return {};
+        }
+    }
 }
--- Commercial/CoordsModel.h
+++ Commercial/CoordsModel.h
@@ -5,9 +5,23 @@
 
 class CoordsModel : public QAbstractTableModel
 {
+    Q_OBJECT
+
 public:
     CoordsModel();
+    virtual ~CoordsModel();
 
+    int rowCount(const QModelIndex &parent = {}) const override;
+    int columnCount(const QModelIndex &parent = {}) const override;
+    QVariant data(const QModelIndex &index = {}, int role = Qt::DisplayRole) const override;
+
+private:
+    std::vector<std::pair<int, int>> m_coordinates{{0, 0}};
+
+    enum CoordRoles {
+        XCoord = Qt::UserRole + 1,
+        YCoord
+    };
 };
 
 #endif // COORDSMODEL_H
