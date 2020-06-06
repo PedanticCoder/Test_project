84564f1919b3ecfe0bd3b4fcbaac88af357639c5
Artem Kurov <55914169+PedanticCoder@users.noreply.github.com>
2020-06-06 12:00:13 +0300

    Add Start Button on a screen

--- Commercial/AppWindow.cpp
+++ Commercial/AppWindow.cpp
@@ -3,6 +3,8 @@
 AppWindow::AppWindow(QWidget *parent)
     : QWidget(parent)
 {
+    m_pStartBtn = new QPushButton(tr("START"), this);
+    m_pStartBtn->setGeometry(10, 10, 150, 25);
 }
 
 AppWindow::~AppWindow()
